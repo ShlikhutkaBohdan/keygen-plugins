617cc195d4a1696c63ae0b862a0d2a86aa6c1809
Shlikhutka.Bohdan <bohdan.shlikhutka@gmail.com>
2021-07-14 19:34:10 +0300

    Include bip_utils configs into coin service inline for Denarius.

--- test/test_get_address.py
+++ test/test_get_address.py
@@ -1,16 +1,21 @@
-from crypto_coin_factory import CoinFactoryExtended
-
-
-def get_coin_address(currency, private_key):
-    service = CoinFactoryExtended().get_coin_service(currency)
-    return service.get_address(private_key)
-
+from plugins.d_crypto_coin_service import DenariusCoinService
 
 def test_get_address_d0():
-    address = get_coin_address('D', 'QSDoM1x6bu7BJH965SEu2GrGZdqXbjfBrjDYxfmfvkGYX7aCAPvX')
+    address = DenariusCoinService().get_address('QSDoM1x6bu7BJH965SEu2GrGZdqXbjfBrjDYxfmfvkGYX7aCAPvX')
     assert 'DPNsvdVer4LPjPApt8hw4sFfwRfb7ihAHj' == address
 
 
 def test_get_address_d1():
-    address = get_coin_address('D', 'QWHQY5iNj5HbS9M25yb2UVm8Lh8aqfJUuqbJ4NhkHQqLcczqL6Qd')
+    address = DenariusCoinService().get_address('QWHQY5iNj5HbS9M25yb2UVm8Lh8aqfJUuqbJ4NhkHQqLcczqL6Qd')
     assert 'DGkPXfipAat7QswFuZtDJ5CeNiPRQnpzDG' == address
+
+
+def test_gen_coin_from_mnemonic_d0():
+    coin = DenariusCoinService().generate_from_mnemonic('flock marble speed enjoy era chef ecology develop boost man stick glass')
+    assert 'QSDoM1x6bu7BJH965SEu2GrGZdqXbjfBrjDYxfmfvkGYX7aCAPvX' == coin.wif
+    assert 'DPNsvdVer4LPjPApt8hw4sFfwRfb7ihAHj' == coin.address
+
+def test_gen_coin_from_mnemonic_d1():
+    coin = DenariusCoinService().generate_from_mnemonic('salute equal wash will sister auto notable marble perfect private wrap park')
+    assert 'QWHQY5iNj5HbS9M25yb2UVm8Lh8aqfJUuqbJ4NhkHQqLcczqL6Qd' == coin.wif
+    assert 'DGkPXfipAat7QswFuZtDJ5CeNiPRQnpzDG' == coin.address
